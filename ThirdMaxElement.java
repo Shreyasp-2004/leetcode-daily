86264a75915f4623b73f6104493e8d4b1a79add4
Shreyas Palsodkar <221478802+Shreyasp-2004@users.noreply.github.com>
2026-01-05 00:00:38 +0530

    Solved Third Maximum element in array

---
+++ ThirdMaxElement.java
@@ -0,0 +1,29 @@
+public class ThirdMaxElement {
+  public int thirdMax(int[] nums) {
+        Long first = null, second = null, third = null;
+
+        for (int x : nums) {
+            long n = x;
+
+            if ((first != null && n == first) || 
+                (second != null && n == second) || 
+                (third != null && n == third)) {
+                continue; 
+            }
+
+            if (first == null || n > first) {
+                third = second;
+                second = first;
+                first = n;
+            } else if (second == null || n > second) {
+                third = second;
+                second = n;
+            } else if (third == null || n > third) {
+                third = n;
+            }
+        }
+
+        return third == null ? first.intValue() : third.intValue();
+    }
+    
+}
